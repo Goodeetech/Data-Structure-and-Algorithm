2a2832bdce36ae6947ec3c8625f816968e7d4a9a
Goodeetech <ayomidegoodee@gmail.com>
2025-06-12 10:37:39 +0100

    started the doublyLinkedList

--- index.js
+++ index.js
@@ -59,326 +59,342 @@
 // // car2.accelerate();
 // // car2.brake();
 
-class MyArray {
-  constructor() {
-    this.length = 0;
-    this.data = {};
-  }
-  push(item) {
-    this.data[this.length] = item;
-    this.length++;
-    return item;
-  }
-  pop() {
-    const lastItem = this.data[this.length - 1];
-    delete this.data[this.length - 1];
-    this.length--;
-    return lastItem;
-  }
-  shift() {
-    const firstItem = this.data[0];
+// class MyArray {
+//   constructor() {
+//     this.length = 0;
+//     this.data = {};
+//   }
+//   push(item) {
+//     this.data[this.length] = item;
+//     this.length++;
+//     return item;
+//   }
+//   pop() {
+//     const lastItem = this.data[this.length - 1];
+//     delete this.data[this.length - 1];
+//     this.length--;
+//     return lastItem;
+//   }
+//   shift() {
+//     const firstItem = this.data[0];
 
-    for (let i = 0; i < this.data.length; i++) {
-      this.data[i] = this.data[i + 1];
-    }
-    delete this.data[this.length - 1];
-    this.length--;
-    return firstItem;
-  }
+//     for (let i = 0; i < this.data.length; i++) {
+//       this.data[i] = this.data[i + 1];
+//     }
+//     delete this.data[this.length - 1];
+//     this.length--;
+//     return firstItem;
+//   }
 
-  unshift(item) {
-    for (let i = this.length; i > 0; i--) {
-      this.data[i] = this.data[i - 1];
-    }
-    this.data[0] = item;
+//   unshift(item) {
+//     for (let i = this.length; i > 0; i--) {
+//       this.data[i] = this.data[i - 1];
+//     }
+//     this.data[0] = item;
 
-    this.length++;
-  }
-  insert(index, item) {
-    for (let i = this.length; i >= index; i--) {
-      this.data[i] = this.data[i - 1];
-    }
-    this.data[index] = item;
-    this.length++;
-  }
+//     this.length++;
+//   }
+//   insert(index, item) {
+//     for (let i = this.length; i >= index; i--) {
+//       this.data[i] = this.data[i - 1];
+//     }
+//     this.data[index] = item;
+//     this.length++;
+//   }
 
-  deleteByIndex(index) {
-    const itemWithIndex = this.data[index]; // Save the item you’re about to delete
+//   deleteByIndex(index) {
+//     const itemWithIndex = this.data[index]; // Save the item you’re about to delete
 
-    for (let i = index; i < this.length; i++) {
-      this.data[i] = this.data[i + 1]; // Shift everything left
-    }
+//     for (let i = index; i < this.length; i++) {
+//       this.data[i] = this.data[i + 1]; // Shift everything left
+//     }
 
-    delete this.data[this.length - 1]; // Delete the last duplicate
-    this.length--; // Decrease the length of your array
+//     delete this.data[this.length - 1]; // Delete the last duplicate
+//     this.length--; // Decrease the length of your array
 
-    return itemWithIndex; // Return the deleted item
-  }
-}
+//     return itemWithIndex; // Return the deleted item
+//   }
+// }
 
-const myNewArray = new MyArray();
-myNewArray.push("food");
-myNewArray.push("Amala");
-myNewArray.push("Asharo");
-console.log(myNewArray);
-myNewArray.unshift("alausa");
-myNewArray.insert(1, "goods");
-myNewArray.pop();
-console.log(myNewArray);
+// const myNewArray = new MyArray();
+// myNewArray.push("food");
+// myNewArray.push("Amala");
+// myNewArray.push("Asharo");
+// console.log(myNewArray);
+// myNewArray.unshift("alausa");
+// myNewArray.insert(1, "goods");
+// myNewArray.pop();
+// console.log(myNewArray);
 
-////STRING
-const reverseString = (str) => str.split("").reverse().join("");
+// ////STRING
+// const reverseString = (str) => str.split("").reverse().join("");
 
-console.log(reverseString("hello"));
-console.log(reverseString("Goodee"));
+// console.log(reverseString("hello"));
+// console.log(reverseString("Goodee"));
 
-const checkPalindrome = (str) => {
-  const string = str.split("").reverse().join("");
-  if (string === str) {
-    console.log("Palindrome");
-  } else {
-    console.log("Not A Palindrome");
-  }
-};
+// const checkPalindrome = (str) => {
+//   const string = str.split("").reverse().join("");
+//   if (string === str) {
+//     console.log("Palindrome");
+//   } else {
+//     console.log("Not A Palindrome");
+//   }
+// };
 
-checkPalindrome("hello");
-checkPalindrome("abba");
+// checkPalindrome("hello");
+// checkPalindrome("abba");
 
-const reverseInt = (str) => {
-  const num = str.toString().split("").reverse().join("");
-  const reversedNum = parseInt(num) * Math.sign(str);
-  return reversedNum;
-};
+// const reverseInt = (str) => {
+//   const num = str.toString().split("").reverse().join("");
+//   const reversedNum = parseInt(num) * Math.sign(str);
+//   return reversedNum;
+// };
 
-console.log(reverseInt(1234));
+// console.log(reverseInt(1234));
 
-const fizzBuzz1 = (firstNum, lastNum) => {
-  for (let i = firstNum; i <= lastNum; i++) {
-    if (i % 5 == 0 && i % 3 == 0) {
-      console.log("fizzBuzz");
-    } else if (i % 5 == 0) {
-      console.log("Buzz");
-    } else if (i % 3 == 0) {
-      console.log("Fizz");
-    } else {
-      console.log(i);
-    }
-  }
-};
+// const fizzBuzz1 = (firstNum, lastNum) => {
+//   for (let i = firstNum; i <= lastNum; i++) {
+//     if (i % 5 == 0 && i % 3 == 0) {
+//       console.log("fizzBuzz");
+//     } else if (i % 5 == 0) {
+//       console.log("Buzz");
+//     } else if (i % 3 == 0) {
+//       console.log("Fizz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
 
-const fizzBuzz = (firstNum, lastNum) => {
-  for (let i = firstNum; i <= lastNum; i++) {
-    if (i % 5 == 0 && i % 3 == 0) {
-      console.log("fizzBuzz");
-    } else if (i % 5 == 0) {
-      console.log("Buzz");
-    } else if (i % 3 == 0) {
-      console.log("Fizz");
-    } else {
-      console.log(i);
-    }
-  }
-};
+// const fizzBuzz = (firstNum, lastNum) => {
+//   for (let i = firstNum; i <= lastNum; i++) {
+//     if (i % 5 == 0 && i % 3 == 0) {
+//       console.log("fizzBuzz");
+//     } else if (i % 5 == 0) {
+//       console.log("Buzz");
+//     } else if (i % 3 == 0) {
+//       console.log("Fizz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
 
-fizzBuzz(1, 20);
+// fizzBuzz(1, 20);
 
-const chunk = (array, size) => {
-  const chunked = [];
-  let index = 0;
+// const chunk = (array, size) => {
+//   const chunked = [];
+//   let index = 0;
 
-  while (index < array.length) {
-    const chunk = array.slice(index, index + size);
-    chunked.push(chunk);
-    index += size;
-  }
-  return chunked;
-};
+//   while (index < array.length) {
+//     const chunk = array.slice(index, index + size);
+//     chunked.push(chunk);
+//     index += size;
+//   }
+//   return chunked;
+// };
 
-console.log(chunk([1, 2, 3, 4, 5, 6, 7], 3));
+// console.log(chunk([1, 2, 3, 4, 5, 6, 7], 3));
 
-const findIndex = (array, target) => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] + array[j] === target) {
-        return [i, j]; // return the indexes
-      }
-    }
-  }
-  return null; // If no match found
-};
+// const findIndex = (array, target) => {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[i] + array[j] === target) {
+//         return [i, j]; // return the indexes
+//       }
+//     }
+//   }
+//   return null; // If no match found
+// };
 
-console.log(findIndex([2, 3, 6, 8, 11, 15], 9)); // ➜ [0, 1]
+// console.log(findIndex([2, 3, 6, 8, 11, 15], 9)); // ➜ [0, 1]
 
-//Linked List
+// //Linked List
+
+// class Node {
+//   constructor(value) {
+//     this.head = value;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor(value) {
+//     this.head = new Node(value);
+//     this.tail = this.head;
+//     this.length = 1;
+//   }
+//   push(value) {
+//     const newNode = new Node(value);
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     }
+//     this.tail.next = newNode;
+//     this.tail = newNode;
+//     this.length++;
+//   }
+//   pop() {
+//     if (!this.head) {
+//       return undefined;
+//     }
+//     let temp = this.head;
+//     let prev = this.head;
+
+//     while (temp.next) {
+//       prev = temp;
+//       temp = prev.next;
+//     }
+
+//     this.tail = prev;
+//     this.tail.next = null;
+//     this.length--;
+
+//     if (this.length === 0) {
+//       this.head = null;
+//       this.tail = null;
+//     }
+//     return temp;
+//   }
+//   unshift(value) {
+//     const newNode = new Node(value);
+
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     } else {
+//       newNode.next = this.head;
+//       this.head = newNode;
+//     }
+
+//     this.length++;
+//     return this;
+//   }
+//   shift() {
+//     if (!this.head) {
+//       return undefined;
+//     }
+//     const newHead = this.head.next;
+//     const removedNode = this.head;
+//     if (this.length == 0) {
+//       this.tail = null;
+//     }
+
+//     this.head = newHead;
+//     this.length--;
+//     return removedNode;
+//   }
+//   getFirst() {
+//     if (!this.head) {
+//       return undefined;
+//     }
+//     return this.head;
+//   }
+//   getLast() {
+//     if (!this.head) {
+//       return undefined;
+//     }
+//     let temp = this.head;
+
+//     while (temp.next) {
+//       temp = temp.next;
+//     }
+//     return temp;
+//     // return this.tail || undefined;
+//   }
+//   get(index) {
+//     let counter = 0;
+//     let temp = this.head;
+//     while (temp) {
+//       if (counter == index) {
+//         return temp;
+//       }
+
+//       counter++;
+//       temp = temp.next;
+//     }
+//     return null;
+//   }
+
+//   set(index, value) {
+//     let counter = 0;
+//     let temp = this.head;
+
+//     while (temp) {
+//       if (counter === index) {
+//         temp.value = value;
+//         return true;
+//       }
+//       temp = temp.next;
+//       counter++;
+//     }
+//     return false;
+//   }
+
+//   insert(index, value) {
+//     const newNode = new Node(value);
+
+//     if (index == 0) {
+//       newNode.next = this.head;
+//       this.head = newNode;
+//       this.length++;
+//       return true;
+//     }
+//     if (index == this.length) {
+//       this.length++;
+//       return this.push(value);
+//     }
+
+//     let temp = this.head;
+//     let counter = 0;
+
+//     while (temp && counter < index - 1) {
+//       temp = temp.next;
+//       counter++;
+//     }
+//     if (!temp) return false;
+//     newNode.next = temp.next;
+//     temp.next = newNode;
+//     this.length++;
+//     return true;
+//   }
+//   size() {
+//     let temp = this.head;
+//     let counter = 0;
+
+//     if (!temp) {
+//       return undefined;
+//     }
+
+//     while (temp) {
+//       temp = temp.next;
+//       counter++;
+//     }
+//     return counter;
+//   }
+//   clear() {
+//     this.head = null;
+//   }
+// }
+
+// const myNewLinkedList = new LinkedList(1);
+// myNewLinkedList.push(10);
+// myNewLinkedList.push(20);
+// myNewLinkedList.push(30);
+// console.log(myNewLinkedList.clear());
 
 class Node {
   constructor(value) {
     this.head = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
-  push(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    }
-    this.tail.next = newNode;
-    this.tail = newNode;
-    this.length++;
-  }
-  pop() {
-    if (!this.head) {
-      return undefined;
-    }
-    let temp = this.head;
-    let prev = this.head;
-
-    while (temp.next) {
-      prev = temp;
-      temp = prev.next;
-    }
-
-    this.tail = prev;
-    this.tail.next = null;
-    this.length--;
-
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return temp;
-  }
-  unshift(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-
-    this.length++;
-    return this;
-  }
-  shift() {
-    if (!this.head) {
-      return undefined;
-    }
-    const newHead = this.head.next;
-    const removedNode = this.head;
-    if (this.length == 0) {
-      this.tail = null;
-    }
-
-    this.head = newHead;
-    this.length--;
-    return removedNode;
-  }
-  getFirst() {
-    if (!this.head) {
-      return undefined;
-    }
-    return this.head;
-  }
-  getLast() {
-    if (!this.head) {
-      return undefined;
-    }
-    let temp = this.head;
-
-    while (temp.next) {
-      temp = temp.next;
-    }
-    return temp;
-    // return this.tail || undefined;
-  }
-  get(index) {
-    let counter = 0;
-    let temp = this.head;
-    while (temp) {
-      if (counter == index) {
-        return temp;
-      }
-
-      counter++;
-      temp = temp.next;
-    }
-    return null;
-  }
-
-  set(index, value) {
-    let counter = 0;
-    let temp = this.head;
-
-    while (temp) {
-      if (counter === index) {
-        temp.value = value;
-        return true;
-      }
-      temp = temp.next;
-      counter++;
-    }
-    return false;
-  }
-
-  insert(index, value) {
-    const newNode = new Node(value);
-
-    if (index == 0) {
-      newNode.next = this.head;
-      this.head = newNode;
-      this.length++;
-      return true;
-    }
-    if (index == this.length) {
-      this.length++;
-      return this.push(value);
-    }
-
-    let temp = this.head;
-    let counter = 0;
-
-    while (temp && counter < index - 1) {
-      temp = temp.next;
-      counter++;
-    }
-    if (!temp) return false;
-    newNode.next = temp.next;
-    temp.next = newNode;
-    this.length++;
-    return true;
-  }
-  size() {
-    let temp = this.head;
-    let counter = 0;
-
-    if (!temp) {
-      return undefined;
-    }
-
-    while (temp) {
-      temp = temp.next;
-      counter++;
-    }
-    return counter;
-  }
-  clear() {
-    this.head = null;
-  }
 }
-
-const myNewLinkedList = new LinkedList(1);
-myNewLinkedList.push(10);
-myNewLinkedList.push(20);
-myNewLinkedList.push(30);
-console.log(myNewLinkedList.clear());
