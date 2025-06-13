9ef999fceb1e416f6e53f215557988e84f5bc22f
Goodeetech <ayomidegoodee@gmail.com>
2025-06-13 18:04:23 +0100

    doublyLinked list push and pop

--- index.js
+++ index.js
@@ -397,4 +397,43 @@ class DoublyLinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.tail) {
+      return undefined;
+    }
+    let temp = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = temp.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+
+    this.length--;
+    return temp;
+  }
 }
+
+const myNewDoublyList = new DoublyLinkedList(1);
+myNewDoublyList.push(10);
+myNewDoublyList.push(20);
+myNewDoublyList.push(30);
+myNewDoublyList.push(40);
+myNewDoublyList.pop();
+
+console.log(myNewDoublyList);
